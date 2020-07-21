ac0ee1aea5b994a14e556e8d3f62abfed233a09c
SergioZ96 <Sergiod.zurita@gmail.com>
2020-07-20 22:16:20 -0400

    worked on menu skeleton page/some css

--- templates/menu.js
+++ templates/menu.js
@@ -1,3 +1,44 @@
 let menu = `
-    <h1>Menu</h1>
+
+    <div class="menu-container">
+        <div class="sopas">
+            
+            <div class="sopa-de-mani">
+                <h3>Sopa de Mani</h3>
+                <p></p>
+            </div>
+            <div class="fideos-uchu">
+                <h3>Fideos Uchu</h3>
+                <p></p>
+            </div>
+            <div class="kawi">
+                <h3>Kawi</h3>
+                <p></p>
+            </div>
+            <div class="sopa-de-quinoa">
+                <h3>Sopa de Quinoa</h3>
+                <p></p>
+            </div>
+        </div>
+
+        <div class="platos">
+            
+            <div class="chicharron">
+                <h3>Chicharron</h3>
+                <p></p>
+            </div>
+            <div class="silpancho">
+                <h3>Silpancho</h3>
+                <p></p>
+            </div>
+            <div class="charque">
+                <h3>Charque</h3>
+                <p></p>
+            </div>
+            <div class="pique-macho">
+                <h3>Pique Macho</h3>
+                <p></p>
+            </div>
+        </div>
+    </div>
 `;
