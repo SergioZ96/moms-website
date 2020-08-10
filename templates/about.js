ebb15ead4664acd2394089f75ecc0bb8c9ab2f37
SergioZ96 <Sergiod.zurita@gmail.com>
2020-08-09 22:16:27 -0400

    started about page

--- templates/about.js
+++ templates/about.js
@@ -1,6 +1,15 @@
 let about = {
     render: 
-        ``,
+        `<div id="about-one" class="about-div">
+            <h3>Soy Dunia!</h3>
+        </div>
+        <div id="about-two" class="about-div">
+            <h3>Y Me Encanta Cocinar </h3>
+        </div>
+        <div id="about-three" class="about-div">
+            <h3>Con Amor Desde Bolivia</h3>
+        </div>`,
+
         after_render: async () => {
             return true;
         }
--- templates/menu.js
+++ templates/menu.js
@@ -3,74 +3,76 @@ let menu = {
         `<div class="menu-container">
             <div class="sopas">
                 
-                <div id="sopa-de-mani" class="soup">
+                <div id="sopa-de-mani" class="soup" animated="false">
                     <h3>Sopa de Mani</h3>
+                    <p hidden>sopa-de-mani.jpg</p>
                 </div>
-                <div id="fideos-uchu" class="soup">
+                <div id="fideos-uchu" class="soup" animated="false">
                     <h3>Fideos Uchu</h3>
+                    <p hidden>fideos-uchu.jpg</p>
                 </div>
-                <div id="kawi" class="soup">
+                <div id="kawi" class="soup" animated="false">
                     <h3>Kawi</h3>
+                    <p hidden>kawi.jpeg</p>
                 </div>
-                <div id="sopa-de-quinoa" class="soup">
+                <div id="sopa-de-quinoa" class="soup" animated="false">
                     <h3>Sopa de Quinoa</h3>
+                    <p hidden>sopa-de-quinoa.jpg</p>
                 </div>
             </div>
     
             <div class="platos">
                 
-                <div class="chicharron">
+                <div id="chicharron" class="food" animated="false">
                     <h3>Chicharron</h3>
+                    <p hidden>chicharron.jpg</p>
                 </div>
-                <div class="silpancho">
+                <div id="silpancho" class="food" animated="false">
                     <h3>Silpancho</h3>
+                    <p hidden>silpancho.jpeg</p>
                 </div>
-                <div class="charque">
+                <div id="charque" class="food" animated="false">
                     <h3>Charque</h3>
+                    <p hidden>charque.jpeg</p>
                 </div>
-                <div class="pique-macho">
+                <div id="pique-macho" class="food" animated="false">
                     <h3>Pique Macho</h3>
+                    <p hidden>pique-macho.jpg</p>
                 </div>
             </div>
     
             <div id="food-info">
                 <h2 id="food-name"></h2>
-                
+                <img id="food-image" animated="false" src="data:," alt>
             </div>
         </div>    
         `,
         after_render: async () => {
+            /* Soups */
             document.querySelectorAll(".soup").forEach(item => {
                 item.addEventListener("click", () => {
                 document.getElementById("food-name").innerHTML = item.querySelector("h3").innerText;
+                document.getElementById("food-image").src = `assets/images/${item.querySelector("p").innerText}`;
+                //document.getElementById("food-image").setAttribute("animated", "false");
+                // Tried to setup a fadein for each food item image on click (currently only does for first click)
+                if(item.getAttribute("animated") == "false"){
+                    document.getElementById("food-image").style.animation = "fadeinfood ease 3s";
+                    item.setAttribute("animated", "true"); //modifies animated attribute of soup menu items
+                }
+                
                 })
             });
 
-            var newImage = document.createElement("img");
+            /* Foods */
+            document.querySelectorAll(".food").forEach(item => {
+                item.addEventListener("click", () => {
+                document.getElementById("food-name").innerHTML = item.querySelector("h3").innerText;
+                document.getElementById("food-image").src = `assets/images/${item.querySelector("p").innerText}`;
+                })
+            });
 
-            
         
             return true;
         }
 
 }
-
-/* 
-function setAll(){
-    var soups = document.getElementsByClassName("sopas");
-    //console.log(soups);
-    // ----- Iterating through an HTML collection console logged (soups)
-    //console.log(soups[0].childNodes);
-    var mani = document.querySelector(".sopa-de-mani");
-    mani.addEventListener("click", () => {
-        document.getElementById("food-name").innerHTML = mani[1].innerHTML;
-    });
- 
-    for (let i = 0; i < soups[0].children.length; i++){
-        soups[i].addEventListener("click", () => {
-            document.getElementById("food-name").innerHTML = soups[0].children[i];
-        });
-    }  
-} */
-
-
