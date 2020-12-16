256e678eae05ca0ff4ec7ad588f06ea8437444d4
SergioZ96 <sergiod.zurita@gmail.com>
2020-12-15 22:25:12 -0500

    added background and some styling to homepage

--- redirect.js
+++ redirect.js
@@ -4,31 +4,36 @@
 
 let redirect404 = () => {
     var segmentCount = 0;
+    // returns a string of the Location object (url)
     var location = window.location;
     location.replace(
+      /*
+        Protocol property of the Location interface is a USVString representing the protocol scheme of the URL, including the final ':' - ex: 'http:'
+        Hostname property of the Location interface is a USVString containing the domain of the URL
+        */
       location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') +
       location.pathname.split('/').slice(0, 1 + segmentCount).join('/') + '/?p=/' +
       location.pathname.slice(1).split('/').slice(segmentCount).join('/').replace(/&/g, '~and~') +
       (location.search ? '&q=' + location.search.slice(1).replace(/&/g, '~and~') : '') +
       location.hash
     );
-  }
+}
   
-  let receiveRedirect = () => {
-    (function(location) {
-      if (location.search) {
-        var q = {};
-        location.search.slice(1).split('&').forEach(function(v) {
-          var a = v.split('=');
-          q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&');
-        });
-        if (q.p !== undefined) {
-          window.history.replaceState(null, null,
-            location.pathname.slice(0, -1) + (q.p || '') +
-            (q.q ? ('?' + q.q) : '') +
-            location.hash
-          );
-        }
+let receiveRedirect = () => {
+  (function(location) {
+    if (location.search) {
+      var q = {};
+      location.search.slice(1).split('&').forEach(function(v) {
+        var a = v.split('=');
+        q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&');
+      });
+      if (q.p !== undefined) {
+        window.history.replaceState(null, null,
+          location.pathname.slice(0, -1) + (q.p || '') +
+          (q.q ? ('?' + q.q) : '') +
+          location.hash
+        );
       }
-    }(window.location));
-  }
+    }
+  }(window.location));
+}
--- templates/homepage.js
+++ templates/homepage.js
@@ -1,11 +1,31 @@
 let homepage = {
     render : 
         `<div id="home-wrapper">
+            <div id="first-home-block">
+                <section>
+                    <h1>Hola soy Dunia</h1>
+                    <h1>Y me encanta la cocina</h1>
+                </section>
+                <img src="../assets/images/mom.JPG">
+                
+            </div>
+            <!--
             <h2>Bienvenidos</h2>
+            <div id="main-abouts">
+
+                <div id="about-one" class="about-div">
+                    <h3>¡Soy Dunia!</h3>
+                </div>
+                <div id="about-two" class="about-div">
+                    <h3>Me Encanta Cocinar </h3>
+                </div>
+                <div id="about-three" class="about-div">
+                    <h3>Con Amor Desde Bolivia</h3>
+                </div>
+            </div>
+            -->
          </div>`,
     after_render: async () => {
-        //Bienvenidos Fade 
-        //document.getElementById("home-wrapper").className += 'fade-out';
         
         return true;
     }
